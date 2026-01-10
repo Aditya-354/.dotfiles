939965f8a68003ddccd24ec5508e7befa5d17fc9
Aditya <adigithub354@gmail.com>
2026-01-10 21:30:30 +0530

    Tweaked gruvbox theme

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -1,6 +1,6 @@
 return {
   "ellisonleao/gruvbox.nvim",
-  priority = 1000, -- load before other plugins
+  priority = 1000,
   config = function()
     require("gruvbox").setup({
       contrast = "soft", -- "soft", "medium", "hard"
