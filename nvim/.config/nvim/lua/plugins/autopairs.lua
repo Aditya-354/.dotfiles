23af172cb3dd4b7c23af4fd3bbfa84e44a51625c
Aditya <adigithub354@gmail.com>
2026-01-19 19:16:28 +0530

    Added autopairs and gitsigns plugins for neovim

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("nvim-autopairs").setup({
       check_ts = true, -- works better with treesitter
-      enable_check_bracket_line = false,
+      enable_check_bracket_line = true,
       ignored_next_char = "[%w%.]",
       disable_in_macro = true,
     })
