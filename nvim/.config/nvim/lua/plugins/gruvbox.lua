0aaaf81e166f916ba85ecb7947eb54d16217f7d8
Aditya <adigithub354@gmail.com>
2026-01-26 23:50:16 +0530

    added more wallpapers & changed nvim theme

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -4,6 +4,7 @@ return {
   config = function()
     require("gruvbox").setup({
       contrast = "soft", -- "soft", "medium", "hard"
+      bold = false,
       transparent_mode = true, -- since you like transparent bg
     })
     -- vim.cmd("colorscheme gruvbox")
