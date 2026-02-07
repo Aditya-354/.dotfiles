10395fb27059f396ccf2c4928af172ed00a864f9
Aditya <adigithub354@gmail.com>
2026-02-07 16:12:56 +0530

    Fixed neotree dotfiles toggle

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -12,7 +12,7 @@ return {
       filesystem = {
         filtered_items = {
           visible = true,   -- 👈 this is the key line
-          hide_dotfiles = false,
+          -- hide_dotfiles = false,
           hide_gitignored = false,
         },
       },
