3b0b9ed8626a194a27e211617ac1c8b077567167
Aditya <adigithub354@gmail.com>
2026-03-03 20:48:55 +0530

    changed nvim themes

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -975,24 +975,24 @@ require("gruvbox").setup({
   terminal_colors = true, -- add neovim terminal colors
   undercurl = true,
   underline = true,
-  bold = true,
+  bold = false,
   italic = {
-    strings = true,
-    emphasis = true,
-    comments = true,
-    operators = true,
-    folds = true,
+    strings = false,
+    emphasis = false,
+    comments = false,
+    operators = false,
+    folds = false,
   },
   strikethrough = true,
   invert_selection = false,
   invert_signs = false,
   invert_tabline = false,
   inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "soft", -- can be "hard", "soft" or empty string
+  contrast = "", -- can be "hard", "soft" or empty string
   palette_overrides = {},
   overrides = {},
   dim_inactive = false,
-  transparent_mode = false,
+  transparent_mode = true,
 })
 
 -- vim.cmd("colorscheme rose-pine")
