fe8cb820c5e4e5dd1c2ba53496ae89504cc5c309
Aditya <adigithub354@gmail.com>
2026-03-04 19:34:06 +0530

    changed line moving up/down keybinds

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -296,10 +296,10 @@ vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window heig
 vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
 
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("n", "<A-u>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-i>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("v", "<A-u>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<A-i>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
