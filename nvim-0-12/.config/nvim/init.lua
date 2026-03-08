2acea4c872160c88983d83e50206fddd367db59b
Aditya <adigithub354@gmail.com>
2026-03-08 16:22:23 +0530

    changed nvim theme

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -1026,8 +1026,8 @@ require("gruvbox").setup({
   transparent_mode = true,
 })
 
--- vim.cmd("colorscheme rose-pine")
+vim.cmd("colorscheme rose-pine")
 -- "vim.cmd("colorscheme rose-pine-main")
-vim.cmd("colorscheme gruvbox")
+-- vim.cmd("colorscheme gruvbox")
 -- vim.cmd("colorscheme rose-pine-moon")
 -- vim.cmd("colorscheme rose-pine-dawn")
