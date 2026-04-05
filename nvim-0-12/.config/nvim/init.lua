34796aad8a2c95fdd227af1362a6e1eea9a8e048
Aditya <adigithub354@gmail.com>
2026-04-05 13:19:31 +0530

    changed theme to gruvbox

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -988,11 +988,11 @@ require("gruvbox").setup({
     folds = true,
   },
   strikethrough = true,
-  invert_selection = true,
-  invert_signs = true,
-  invert_tabline = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
   inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "hard", -- can be "hard", "soft" or empty string
+  contrast = "", -- can be "hard", "soft" or empty string
   palette_overrides = {},
   overrides = {},
   dim_inactive = false,
