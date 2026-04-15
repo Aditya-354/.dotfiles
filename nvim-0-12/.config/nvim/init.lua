67f661f3bc508c5c022ccb2e8789d0a3afdd498b
Aditya <adigithub354@gmail.com>
2026-04-15 17:08:24 +0530

    changed default onedark variant to 'deep' for neovim

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -1009,7 +1009,7 @@ require("darkrose").setup({
 -- Lua
 require('onedark').setup  {
     -- Main options --
-    style = 'cool', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    style = 'deep', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
     transparent = false,  -- Show/hide background
     term_colors = true, -- Change terminal color as per the selected theme style
     ending_tildes = true, -- Show the end-of-buffer tildes. By default they are hidden
