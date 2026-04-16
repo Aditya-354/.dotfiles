620755226d243c7094f5fc06a7ad41dc7ae0397e
Aditya <adigithub354@gmail.com>
2026-04-16 10:11:07 +0530

    added new theme plugin [oasis, sonokai

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -1170,7 +1170,7 @@ require("oasis").setup({
   },
 
   -- Display options
-  transparent = false,                  -- Set to true for transparent backgrounds (bye-bye theme backgrounds)
+  transparent = true,                  -- Set to true for transparent backgrounds (bye-bye theme backgrounds)
   terminal_colors = true,               -- Apply Oasis colors to Neovim's built-in terminal
   match_paren_bg = true,               -- Enable/disable background on MatchParen highlight
 
