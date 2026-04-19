e8e20a66bf44b24a0bb167a9b8419dadb9fe5612
Aditya <adigithub354@gmail.com>
2026-04-19 22:25:18 +0530

    changed default rose-pine variant to 'main'

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -1265,7 +1265,7 @@ require("tokyonight").setup({
 
 require("rose-pine").setup({
     variant = "auto", -- auto, main, moon, or dawn
-    dark_variant = "moon", -- main, moon, or dawn
+    dark_variant = "main", -- main, moon, or dawn
     dim_inactive_windows = false,
     extend_background_behind_borders = true,
 
