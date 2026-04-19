6856e460cc244572baa4330c28ef87b1ee97d961
Aditya <adigithub354@gmail.com>
2026-04-19 21:47:28 +0530

    added new theme plugin [nord]

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -435,6 +435,7 @@ packadd("onedark.nvim")
 packadd("nvim-lspconfig")
 packadd("mason.nvim")
 packadd("gruvbox.nvim")
+packadd("nord.nvim")
 packadd("kanagawa.nvim")
 packadd("vscode.nvim")
 packadd("oasis.nvim")
@@ -1239,4 +1240,13 @@ require('kanagawa').setup({
     }
 })
 
+-- Example config in lua
+vim.g.nord_contrast = true
+vim.g.nord_borders = false
+vim.g.nord_disable_background = false
+vim.g.nord_italic = false
+vim.g.nord_uniform_diff_background = true
+vim.g.nord_bold = false
+
 vim.cmd("colorscheme oasis")
+
