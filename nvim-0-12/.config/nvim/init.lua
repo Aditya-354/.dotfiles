7e3039f0e90fbca70e78afbaaedcc0cfe7805419
Aditya <adigithub354@gmail.com>
2026-04-25 22:59:11 +0530

    enabled auto-completion

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -407,12 +407,12 @@ vim.pack.add({
 	-- Language Server Protocols
 	"https://www.github.com/neovim/nvim-lspconfig",
 	"https://github.com/mason-org/mason.nvim",
-	-- "https://github.com/creativenull/efmls-configs-nvim",
-	-- {
-	-- 	src = "https://github.com/saghen/blink.cmp",
-	-- 	version = vim.version.range("1.*"),
-	-- },
-	-- "https://github.com/L3MON4D3/LuaSnip",
+	"https://github.com/creativenull/efmls-configs-nvim",
+	{
+		src = "https://github.com/saghen/blink.cmp",
+		version = vim.version.range("1.*"),
+	},
+	"https://github.com/L3MON4D3/LuaSnip",
 })
 
 -- STRUDEL
@@ -449,9 +449,9 @@ packadd("neomodern.nvim")
 packadd("darkrose.nvim")
 packadd("mellifluous.nvim")
 packadd("everforest")
--- packadd("efmls-configs-nvim")
--- packadd("blink.cmp")
--- packadd("LuaSnip")
+packadd("efmls-configs-nvim")
+packadd("blink.cmp")
+packadd("LuaSnip")
 
 -- ============================================================================
 -- PLUGIN CONFIGS
@@ -737,114 +737,114 @@ vim.keymap.set("n", "<leader>q", function()
 end, { desc = "Open diagnostic list" })
 vim.keymap.set("n", "<leader>dl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
--- require("blink.cmp").setup({
--- 	keymap = {
--- 		preset = "none",
--- 		["<C-Space>"] = { "show", "hide" },
--- 		["<CR>"] = { "accept", "fallback" },
--- 		["<C-j>"] = { "select_next", "fallback" },
--- 		["<C-k>"] = { "select_prev", "fallback" },
--- 		["<Tab>"] = { "snippet_forward", "fallback" },
--- 		["<S-Tab>"] = { "snippet_backward", "fallback" },
--- 	},
--- 	appearance = { nerd_font_variant = "mono" },
--- 	completion = { menu = { auto_show = true } },
--- 	sources = { default = { "lsp", "path", "buffer", "snippets" } },
--- 	snippets = {
--- 		expand = function(snippet)
--- 			require("luasnip").lsp_expand(snippet)
--- 		end,
--- 	},
---
--- 	fuzzy = {
--- 		implementation = "prefer_rust",
--- 		prebuilt_binaries = { download = true },
--- 	},
--- })
---
--- vim.lsp.config["*"] = {
--- 	capabilities = require("blink.cmp").get_lsp_capabilities(),
--- }
---
--- vim.lsp.config("lua_ls", {
--- 	settings = {
--- 		Lua = {
--- 			diagnostics = { globals = { "vim" } },
--- 			telemetry = { enable = false },
--- 		},
--- 	},
--- })
--- vim.lsp.config("pyright", {})
--- vim.lsp.config("bashls", {})
--- vim.lsp.config("ts_ls", {})
--- vim.lsp.config("gopls", {})
--- vim.lsp.config("clangd", {})
+require("blink.cmp").setup({
+	keymap = {
+		preset = "none",
+		["<C-Space>"] = { "show", "hide" },
+		["<CR>"] = { "accept", "fallback" },
+		["<C-j>"] = { "select_next", "fallback" },
+		["<C-k>"] = { "select_prev", "fallback" },
+		["<Tab>"] = { "snippet_forward", "fallback" },
+		["<S-Tab>"] = { "snippet_backward", "fallback" },
+	},
+	appearance = { nerd_font_variant = "mono" },
+	completion = { menu = { auto_show = true } },
+	sources = { default = { "lsp", "path", "buffer", "snippets" } },
+	snippets = {
+		expand = function(snippet)
+			require("luasnip").lsp_expand(snippet)
+		end,
+	},
 
--- do
--- 	local luacheck = require("efmls-configs.linters.luacheck")
--- 	local stylua = require("efmls-configs.formatters.stylua")
---
--- 	local flake8 = require("efmls-configs.linters.flake8")
--- 	local black = require("efmls-configs.formatters.black")
---
--- 	local prettier_d = require("efmls-configs.formatters.prettier_d")
--- 	local eslint_d = require("efmls-configs.linters.eslint_d")
---
--- 	local fixjson = require("efmls-configs.formatters.fixjson")
---
--- 	local shellcheck = require("efmls-configs.linters.shellcheck")
--- 	local shfmt = require("efmls-configs.formatters.shfmt")
---
--- 	local cpplint = require("efmls-configs.linters.cpplint")
--- 	local clangfmt = require("efmls-configs.formatters.clang_format")
---
--- 	local go_revive = require("efmls-configs.linters.go_revive")
--- 	local gofumpt = require("efmls-configs.formatters.gofumpt")
---
--- 	vim.lsp.config("efm", {
--- 		filetypes = {
--- 			"c",
--- 			"cpp",
--- 			"css",
--- 			"go",
--- 			"html",
--- 			"javascript",
--- 			"javascriptreact",
--- 			"json",
--- 			"jsonc",
--- 			"lua",
--- 			"markdown",
--- 			"python",
--- 			"sh",
--- 			"typescript",
--- 			"typescriptreact",
--- 			"vue",
--- 			"svelte",
--- 		},
--- 		init_options = { documentFormatting = true },
--- 		settings = {
--- 			languages = {
--- 				c = { clangfmt, cpplint },
--- 				go = { gofumpt, go_revive },
--- 				cpp = { clangfmt, cpplint },
--- 				css = { prettier_d },
--- 				html = { prettier_d },
--- 				javascript = { eslint_d, prettier_d },
--- 				javascriptreact = { eslint_d, prettier_d },
--- 				json = { eslint_d, fixjson },
--- 				jsonc = { eslint_d, fixjson },
--- 				lua = { luacheck, stylua },
--- 				markdown = { prettier_d },
--- 				python = { flake8, black },
--- 				sh = { shellcheck, shfmt },
--- 				typescript = { eslint_d, prettier_d },
--- 				typescriptreact = { eslint_d, prettier_d },
--- 				vue = { eslint_d, prettier_d },
--- 				svelte = { eslint_d, prettier_d },
--- 			},
--- 		},
--- 	})
--- end
+	fuzzy = {
+		implementation = "prefer_rust",
+		prebuilt_binaries = { download = true },
+	},
+})
+
+vim.lsp.config["*"] = {
+	capabilities = require("blink.cmp").get_lsp_capabilities(),
+}
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = { globals = { "vim" } },
+			telemetry = { enable = false },
+		},
+	},
+})
+vim.lsp.config("pyright", {})
+vim.lsp.config("bashls", {})
+vim.lsp.config("ts_ls", {})
+vim.lsp.config("gopls", {})
+vim.lsp.config("clangd", {})
+
+do
+	local luacheck = require("efmls-configs.linters.luacheck")
+	local stylua = require("efmls-configs.formatters.stylua")
+
+	local flake8 = require("efmls-configs.linters.flake8")
+	local black = require("efmls-configs.formatters.black")
+
+	local prettier_d = require("efmls-configs.formatters.prettier_d")
+	local eslint_d = require("efmls-configs.linters.eslint_d")
+
+	local fixjson = require("efmls-configs.formatters.fixjson")
+
+	local shellcheck = require("efmls-configs.linters.shellcheck")
+	local shfmt = require("efmls-configs.formatters.shfmt")
+
+	local cpplint = require("efmls-configs.linters.cpplint")
+	local clangfmt = require("efmls-configs.formatters.clang_format")
+
+	local go_revive = require("efmls-configs.linters.go_revive")
+	local gofumpt = require("efmls-configs.formatters.gofumpt")
+
+	vim.lsp.config("efm", {
+		filetypes = {
+			"c",
+			"cpp",
+			"css",
+			"go",
+			"html",
+			"javascript",
+			"javascriptreact",
+			"json",
+			"jsonc",
+			"lua",
+			"markdown",
+			"python",
+			"sh",
+			"typescript",
+			"typescriptreact",
+			"vue",
+			"svelte",
+		},
+		init_options = { documentFormatting = true },
+		settings = {
+			languages = {
+				c = { clangfmt, cpplint },
+				go = { gofumpt, go_revive },
+				cpp = { clangfmt, cpplint },
+				css = { prettier_d },
+				html = { prettier_d },
+				javascript = { eslint_d, prettier_d },
+				javascriptreact = { eslint_d, prettier_d },
+				json = { eslint_d, fixjson },
+				jsonc = { eslint_d, fixjson },
+				lua = { luacheck, stylua },
+				markdown = { prettier_d },
+				python = { flake8, black },
+				sh = { shellcheck, shfmt },
+				typescript = { eslint_d, prettier_d },
+				typescriptreact = { eslint_d, prettier_d },
+				vue = { eslint_d, prettier_d },
+				svelte = { eslint_d, prettier_d },
+			},
+		},
+	})
+end
 
 vim.lsp.enable({
 	"lua_ls",
@@ -1343,5 +1343,5 @@ require("rose-pine").setup({
     end,
 })
 
-vim.cmd("colorscheme nord")
+vim.cmd("colorscheme oasis-starlight")
 
