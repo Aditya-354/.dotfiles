a8edb1a8063e1e6c573a1261c89872f8218701d9
Aditya <adigithub354@gmail.com>
2026-04-27 16:46:16 +0530

    added new theme plugin [black-metal-nvim-theme]

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -1,29 +1,5 @@
 vim.opt.termguicolors = true
 
--- local function set_transparent() -- set UI component to transparent
--- 	local groups = {
--- 		"Normal",
--- 		"NormalNC",
--- 		"EndOfBuffer",
--- 		"NormalFloat",
--- 		"FloatBorder",
--- 		"SignColumn",
--- 		"StatusLine",
--- 		"StatusLineNC",
--- 		"TabLine",
--- 		"TabLineFill",
--- 		"TabLineSel",
--- 		"ColorColumn",
--- 	}
--- 	for _, g in ipairs(groups) do
--- 		vim.api.nvim_set_hl(0, g, { bg = "none" })
--- 	end
--- 	-- vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none", fg = "#767676" })
--- end
---
--- vim.cmd("colorscheme torte")
---
--- set_transparent()
 
 -- ============================================================================
 -- OPTIONS
@@ -396,6 +372,7 @@ vim.pack.add({
   "https://github.com/rebelot/kanagawa.nvim",
   "https://github.com/ramojus/mellifluous.nvim",
   "https://github.com/folke/tokyonight.nvim",
+  "https://github.com/metalelf0/black-metal-theme-neovim",
   "https://github.com/shaunsingh/nord.nvim",
   { src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
   {
@@ -440,6 +417,7 @@ packadd("gruvbox.nvim")
 packadd("nord.nvim")
 packadd("rose-pine")
 packadd("tokyonight.nvim")
+packadd("black-metal-theme-neovim")
 packadd("kanagawa.nvim")
 packadd("vscode.nvim")
 packadd("oasis.nvim")
@@ -1315,5 +1293,89 @@ require("rose-pine").setup({
     end,
 })
 
-vim.cmd("colorscheme kanagawa-dragon")
+require("black-metal").setup({
+  -----MAIN OPTIONS-----
+  --
+  -- Can be one of: bathory | burzum | dark-funeral | darkthrone | 
+  -- emperor | gorgoroth | immortal | impaled-nazarene | khold | marduk | mayhem | nile | taake | thyrfing | venom | windir
+  theme = "burzum",
+  cursorline_gutter = true,
+  -- If true, highlights the gutter darker than the bg
+  dark_gutter = false,
+  -- if true favor treesitter highlights over semantic highlights
+  favor_treesitter_hl = false,
+  -- Don't set background of floating windows. Recommended for when using floating
+  -- windows with borders.
+  plain_float = false,
+  -- Show the end-of-buffer character
+  show_eob = true,
+  -- If true, enable the vim terminal colors
+  term_colors = true,
+  -- Keymap (in normal mode) to toggle between light and dark variants.
+  toggle_variant_key = "<leader>bt",
+  -- Don't set background
+  transparent = true,
+  alt_bg = false,
+
+  diagnostics = {
+    darker = true, -- Darker colors for diagnostic
+    undercurl = true, -- Use undercurl for diagnostics
+    background = true, -- Use background color for virtual text
+  },
+  -- The following table accepts values the same as the `gui` option for normal
+  -- highlights. For example, `bold`, `italic`, `underline`, `none`.
+  code_style = {
+    comments = "italic",
+    conditionals = "none",
+    functions = "none",
+    keywords = "none",
+    headings = "none", -- Markdown headings
+    operators = "none",
+    keyword_return = "none",
+    strings = "none",
+    variables = "none",
+  },
+
+  -- The following options allow for more control over some plugin appearances.
+  plugin = {
+    lualine = {
+      -- Bold lualine_a sections
+      bold = true,
+      -- Don't set section/component backgrounds. Recommended to not set
+      -- section/component separators.
+      plain = false,
+    },
+    cmp = { -- works for nvim.cmp and blink.nvim
+      -- Don't highlight lsp-kind items. Only the current selection will be highlighted.
+      plain = false,
+      -- Reverse lsp-kind items' highlights in blink/cmp menu.
+      reverse = false,
+    },
+  },
+})
+
+local function set_transparent() -- set UI component to transparent
+	local groups = {
+		"Normal",
+		"NormalNC",
+		"EndOfBuffer",
+		"NormalFloat",
+		"FloatBorder",
+		"SignColumn",
+		"StatusLine",
+		"StatusLineNC",
+		"TabLine",
+		"TabLineFill",
+		"TabLineSel",
+		"ColorColumn",
+	}
+	for _, g in ipairs(groups) do
+		vim.api.nvim_set_hl(0, g, { bg = "none" })
+	end
+	-- vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none", fg = "#767676" })
+end
+
+vim.cmd("colorscheme burzum")
+
+set_transparent()
 
