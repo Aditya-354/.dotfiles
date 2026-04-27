e5f6375efcd3c6e8a2f8db43ad1edd110357b908
Aditya <adigithub354@gmail.com>
2026-04-27 19:54:23 +0530

    removed bg from LineNr

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -1368,9 +1368,11 @@ local function set_transparent() -- set UI component to transparent
 		"TabLineFill",
 		"TabLineSel",
 		"ColorColumn",
+    "LineNr",
+    -- "CursorLineNr",
 	}
 	for _, g in ipairs(groups) do
-		vim.api.nvim_set_hl(0, g, { bg = "none" })
+		vim.api.nvim_set_hl(0, g, { bg = "none", ctermbg = "none" })
 	end
 	-- vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none", fg = "#767676" })
 end
