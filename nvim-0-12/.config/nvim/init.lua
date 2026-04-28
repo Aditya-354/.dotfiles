95a75ec14b940c3cfa62270d5adf35ff6b46760e
Aditya <adigithub354@gmail.com>
2026-04-28 19:40:53 +0530

    set lualine theme to rose-pine

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -1050,7 +1050,7 @@ require("mellifluous").setup({
 require("lualine").setup({
     options = {
         icons_enabled = true,
-        theme = 'auto',
+        theme = 'rose-pine',
         -- component_separators = { left = '', right = ''},
         -- section_separators = { left = '', right = ''},
         component_separators = { left = '', right = '' },
