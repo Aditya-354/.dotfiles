883dde64d55b0bf4b56c33978428c7686cd5bed5
Aditya <adigithub354@gmail.com>
2026-06-09 21:44:43 +0530

    .

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -1234,6 +1234,32 @@ require("solarized-osaka").setup({
   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 })
 
+require('onenord').setup({
+  theme = nil, -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
+  borders = true, -- Split window borders
+  fade_nc = false, -- Fade non-current windows, making them more distinguishable
+  styles = {
+    comments = "italic",
+    strings = "NONE",
+    keywords = "NONE",
+    functions = "NONE",
+    variables = "NONE",
+    diagnostics = "underline",
+  },
+  disable = {
+    background = false, -- Disable setting the background color
+    float_background = false, -- Disable setting the background color for floating windows
+    cursorline = true, -- Disable the cursorline
+    eob_lines = false, -- Hide the end-of-buffer lines
+  },
+  -- Inverse highlight for different groups
+  inverse = {
+    match_paren = false,
+  },
+  custom_highlights = {}, -- Overwrite default highlight groups
+  custom_colors = {}, -- Overwrite default colors
+})
+
 require("catppuccin").setup({
     flavour = "auto", -- latte, frappe, macchiato, mocha
     background = { -- :h background
