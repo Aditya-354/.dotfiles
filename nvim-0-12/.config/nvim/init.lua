3aac76b9ee16984e9454e1b1be22660697a9c418
Aditya <adigithub354@gmail.com>
2026-06-12 13:04:59 +0530

    .

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -1247,7 +1247,7 @@ require('onenord').setup({
     diagnostics = "underline",
   },
   disable = {
-    background = true, -- Disable setting the background color
+    background = false, -- Disable setting the background color
     float_background = false, -- Disable setting the background color for floating windows
     cursorline = true, -- Disable the cursorline
     eob_lines = false, -- Hide the end-of-buffer lines
