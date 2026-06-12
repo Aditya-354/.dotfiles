bfac90debeddb3104d60bb8a7b4ac6f23ccef906
Aditya <adigithub354@gmail.com>
2026-06-12 13:01:18 +0530

    .

--- hypr/.config/hypr/modules/decorations.lua
+++ hypr/.config/hypr/modules/decorations.lua
@@ -3,10 +3,10 @@
 -----------------------
 hl.config({
     general = {
-        -- gaps_in  = 8,
-        -- gaps_out = 15,
-        gaps_in = 0,
-        gaps_out = 0,
+        gaps_in  = 5,
+        gaps_out = 28,
+        -- gaps_in = 0,
+        -- gaps_out = 0,
 
         border_size = 1,
 
