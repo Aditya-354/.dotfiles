9606eb254eb5b2e44248d09ae5191a9d830083dc
Aditya <adigithub354@gmail.com>
2026-06-23 18:57:03 +0530

    .

--- nvim-0-12/.config/nvim/init.lua
+++ nvim-0-12/.config/nvim/init.lua
@@ -553,17 +553,8 @@ vim.keymap.set("n", "<leader>fX", function()
 	require("fzf-lua").diagnostics_workspace()
 end, { desc = "FZF Diagnostics Workspace" })
 
-require("mini.ai").setup({})
--- require("mini.comment").setup({})
--- require("mini.move").setup({})
-require("mini.surround").setup({})
--- require("mini.cursorword").setup({})
 require("mini.indentscope").setup({})
--- require("mini.pairs").setup({})
--- require("mini.trailspace").setup({})
-require("mini.bufremove").setup({})
--- require("mini.notify").setup({})
-require("mini.icons").setup({})
+require("mini.notify").setup({})
 
 require("gitsigns").setup({
 	signs = {
