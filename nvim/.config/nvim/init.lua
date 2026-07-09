c9fde6b26599bccb2e3fea994a3c2f463dea86a7
Aditya <adigithub354@gmail.com>
2026-07-09 12:13:16 +0530

    .

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -543,28 +543,6 @@ end
 
 setup_treesitter()
 
--- require("nvim-tree").setup({
--- 	view = {
--- 		width = 35,
--- 	},
--- 	filters = {
--- 		dotfiles = false,
--- 	},
--- 	renderer = {
--- 		group_empty = true,
--- 	},
--- })
--- vim.keymap.set("n", "<leader>j", function()
--- 	require("nvim-tree.api").tree.toggle()
--- end, { desc = "Toggle NvimTree" })
---
--- vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "none" })
--- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NvimTreeSignColumn", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NvimTreeWinSeparator", { fg = "#2a2a2a", bg = "none" })
--- vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { bg = "none" })
-
 require("fzf-lua").setup({})
 
 vim.keymap.set("n", "<leader>ff", function()
