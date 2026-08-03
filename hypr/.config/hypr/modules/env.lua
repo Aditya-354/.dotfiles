c8b1e1a9fcc85001b93b7ce9c252a7a325273299
Aditya <adigithub354@gmail.com>
2026-08-03 18:55:07 +0530

    .

--- hypr/.config/hypr/modules/env.lua
+++ hypr/.config/hypr/modules/env.lua
@@ -3,7 +3,8 @@
 -------------------------------
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
--- hl.env("GTK_THEME", "orchis-dark")
+hl.env("XCURSOR_THEME", "Capitaine Cursors(Gruvbox)")
+hl.env("HYPRCURSOR_THEME", "Capitaine Cursors(Gruvbox)")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
