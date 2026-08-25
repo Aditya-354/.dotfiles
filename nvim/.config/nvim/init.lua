b92769d1d784de8ab42f0c76b21c435fa516fc77
Aditya <adigithub354@gmail.com>
2026-08-25 10:10:09 +0530

    .

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1004,33 +1004,33 @@ require("gruber-darker").setup({
 --     end,
 -- })
 
--- vim.api.nvim_create_autocmd("ColorScheme", {
---     pattern = "retrobox",
---     callback = function()
---         local hl_groups = {
---             "Normal",
---             "NormalFloat",
---             "SignColumn",
---             "NormalNC", -- background for non-current windows
---             "EndOfBuffer",
---             "MsgArea",
---             "FloatBorder",
---             "StatusLine",
---             "StatusLineNC",
---             "ColorColumn",
---             "TabLine",
---             "TabLineFill",
---             "TabLineSel",
---             "LineNr",
---             "Cursor",
---             "CursorLine",
---             "CursorLineNr",
---         }
---         for _, group in ipairs(hl_groups) do
---             vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
---         end
---     end,
--- })
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "retrobox",
+    callback = function()
+        local hl_groups = {
+            "Normal",
+            "NormalFloat",
+            "SignColumn",
+            "NormalNC", -- background for non-current windows
+            "EndOfBuffer",
+            "MsgArea",
+            "FloatBorder",
+            "StatusLine",
+            "StatusLineNC",
+            "ColorColumn",
+            "TabLine",
+            "TabLineFill",
+            "TabLineSel",
+            "LineNr",
+            "Cursor",
+            "CursorLine",
+            "CursorLineNr",
+        }
+        for _, group in ipairs(hl_groups) do
+            vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+        end
+    end,
+})
 
 vim.api.nvim_create_autocmd("ColorScheme", {
     pattern = "*",
