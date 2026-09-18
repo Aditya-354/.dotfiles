d9d74fcf632eba3d2b5d7d076b244f8c71cf9417
Aditya <adigithub354@gmail.com>
2026-09-18 17:26:48 +0530

    .

--- hypr/.config/hypr/modules/decorations.lua
+++ hypr/.config/hypr/modules/decorations.lua
@@ -38,7 +38,7 @@ hl.config({
         },
 
         blur = {
-            enabled   = true,
+            enabled   = false,
             size      = 1,
             passes    = 2,
             vibrancy  = 0.1696,
